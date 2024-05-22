93d73e6929187aff18fc67ebc37e3fddb22eb703
kevin25053 <58118608+kevin25053@users.noreply.github.com>
2024-05-22 21:53:31 +0300

    Update articles.js

--- articles.js
+++ articles.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const fs = require('fs').promises;
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const path = require('path');
 var pth=''
 pth=path.join(__dirname);
